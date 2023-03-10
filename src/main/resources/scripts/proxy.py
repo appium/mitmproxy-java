30b2f65935f5bc912725d23e4c05ffcb983f16e7
himanshunikhare25 <himanshunikhare@outlook.com>
2023-03-10 11:43:16 +0530

    The mitmproxy API seems to have changed and the syntax is now http.Response.make

--- src/main/resources/scripts/proxy.py
+++ src/main/resources/scripts/proxy.py
@@ -150,7 +150,7 @@ class WebSocketAdapter:
             new_metadata = message_response[0]
             new_body = message_response[1]
 
-            flow.response = http.HTTPResponse.make(
+            flow.response = http.Response.make(
                 new_metadata['status_code'],
                 new_body,
                 map(convert_headers_to_bytes, new_metadata['headers'])
@@ -198,7 +198,7 @@ class WebSocketAdapter:
 
         #print("Prepping response!")
 
-        flow.response = http.HTTPResponse.make(
+        flow.response = http.Response.make(
             new_metadata['status_code'],
             new_body,
             map(convert_headers_to_bytes, new_metadata['headers'])
